c8767f2a1891bfb7df7f870ce1b307f46ed3d2e8
Ankitkandel <142578400+Ankitkandel@users.noreply.github.com>
2024-12-08 13:24:29 +0000

    Update app.js

--- web-app/app.js
+++ web-app/app.js
@@ -3,25 +3,26 @@ document.getElementById('uploadForm').addEventListener('submit', async function 
     
     // Get the file name and content from the form inputs
     const fileName = document.getElementById('fileName').value;
-    const fileContent = document.getElementById('fileContent').value;
+    const fileContent = document.getElementById('fileContent').files[0];
 
     // Check if both fields are filled
     if (!fileName || !fileContent) {
-        document.getElementById('responseMessage').innerHTML = 'Please provide both a file name and file content.';
+        document.getElementById('responseMessage').innerHTML = 'Please provide both a file name and select a file.';
         return;
     }
+
+    const formData = new FormData();
+    formData.append('fileName', fileName);
+    formData.append('fileContent', fileContent);
 
     try {
         // Make a POST request to your Azure Function
         const response = await fetch('https://touristmediaapi.azurewebsites.net/api/upload?code=<Your-Master-Host-Key>', {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json'
+                'Accept': 'application/json',
             },
-            body: JSON.stringify({
-                fileName: fileName,
-                fileContent: fileContent
-            })
+            body: formData
         });
 
         const data = await response.json();
